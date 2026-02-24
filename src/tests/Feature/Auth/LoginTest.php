68a450f2d33d44e5e287046c78db5ca3f9006d39
sanwon-lee <200340413+sanwon-lee@users.noreply.github.com>
2026-02-24 09:03:48 +0900

    refactor: move authentication tests to sub-directory
    - Relocate the followings from tests/Feature to tests/Feature/Auth
    - LoginTest.php
    - RegistrationTest.php

--- src/tests/Feature/Auth/LoginTest.php
+++ src/tests/Feature/Auth/LoginTest.php
@@ -3,7 +3,9 @@
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 
+use function Pest\Laravel\actingAs;
 use function Pest\Laravel\assertAuthenticatedAs;
+use function Pest\Laravel\assertGuest;
 use function Pest\Laravel\get;
 use function Pest\Laravel\post;
 
@@ -23,4 +25,14 @@ it('can authenticate users using the login screen', function () {
 
     assertAuthenticatedAs($user);
     $response->assertRedirect('/home');
+});
+
+it('can logout', function () {
+    $user = User::factory()->create();
+
+    actingAs($user)
+        ->post('/logout')
+        ->assertRedirect('/');
+
+    assertGuest();
 });
