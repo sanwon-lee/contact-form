c4abee7130ee37ba300726805310c89ca1896821
sanwon-lee <200340413+sanwon-lee@users.noreply.github.com>
2026-02-27 01:19:53 +0900

    feat: finish StoreContactRequest

--- src/app/Http/Requests/StoreContactRequest.php
+++ src/app/Http/Requests/StoreContactRequest.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests;
 
+use App\Models\Category;
 use App\Models\Contact;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Validator;
@@ -29,7 +30,7 @@ class StoreContactRequest extends FormRequest
             Contact::COL_LAST_NAME   => ['required', 'string',],
             Contact::COL_GENDER      => ['required',],
             Contact::COL_EMAIL       => ['required', 'email',],
-            Contact::COL_TEL         => ['required', 'integer', 'max_digits:5',],
+            Contact::COL_TEL         => ['required', 'regex:/^[0-9]+$/u', 'max_digits:5',],
             Contact::COL_ADDRESS     => ['required',],
             Contact::COL_DETAIL      => ['required', 'max:120'],
         ];
@@ -48,5 +49,29 @@ class StoreContactRequest extends FormRequest
                 );
             }
         });
+    }
+
+    public function attributes(): array
+    {
+        return [
+            Contact::COL_CATEGORY_ID => Category::getLabel(Category::COL_CONTENT),
+            Contact::COL_FIRST_NAME  => '姓',
+            Contact::COL_LAST_NAME   => '名',
+            Contact::COL_GENDER      => '性別',
+            Contact::COL_EMAIL       => 'メールアドレス',
+            Contact::COL_TEL         => '電話番号',
+            Contact::COL_ADDRESS     => '住所',
+            Contact::COL_DETAIL      => 'お問い合わせ内容',
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            Contact::COL_GENDER . '.required'      => ':attributeを選択してください',
+            Contact::COL_TEL . '.regex'            => ':attributeは半角英数字で入力してください',
+            Contact::COL_TEL . '.max_digits'       => ':attributeは5桁まで数字で入力してください',
+            Contact::COL_CATEGORY_ID . '.required' => ':attributeを選択してください',
+        ];
     }
 }
